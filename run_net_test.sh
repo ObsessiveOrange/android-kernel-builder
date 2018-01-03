59e72f0e761862dcf0b23afdd26d0fed1b15a5a8
Benedict Wong <benedictwong@google.com>
2018-01-03 14:34:55 -0800

    Added privileged flag to work with ptrace on debian

--- run_net_test.sh
+++ run_net_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -v /dev/shm:/dev/shm -v $KERNEL_REPO_HOME:/data/kernel -v $ANDROID_REPO_HOME:/data/aosp k-builder /data/aosp/kernel/tests/net/test/run_net_test.sh "$@"
+docker run -it --rm --privileged -v /dev/shm:/dev/shm -v $KERNEL_REPO_HOME:/data/kernel -v $ANDROID_REPO_HOME:/data/aosp k-builder /data/aosp/kernel/tests/net/test/run_net_test.sh "$@"
