00f2d275c53e965f724a43500d063ba66f21518d
Benedict Wong <benedictwong@google.com>
2018-01-03 17:41:41 -0800

    Added GDB, UML dependencies, and set container name

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM ubuntu:14.04
 RUN mkdir /data
 
 RUN apt-get update
-RUN apt-get install -y build-essential bc wget
+RUN apt-get install -y build-essential bc wget gdb libssl-dev
 
 WORKDIR /data/kernel
--- run_net_test.sh
+++ run_net_test.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-docker run -it --rm --privileged -v /dev/shm:/dev/shm -v $KERNEL_REPO_HOME:/data/kernel -v $ANDROID_REPO_HOME:/data/aosp k-builder /data/aosp/kernel/tests/net/test/run_net_test.sh "$@"
+function print-error() { printf '\e[31m%s\e[0m\n' "$1"; }
+function check-var-set() {
+ if [ -z "${!1}" ]; then
+   print-error "Variable $1 must be provided."
+   print-error "To set it, run \"export $1=<value>\", then try again";
+   return -1
+fi
+}
+
+check-var-set KERNEL_REPO_HOME || exit -1
+check-var-set ANDROID_REPO_HOME || exit -1
+
+docker run --name uml-android -it --rm --privileged -v /dev/shm:/dev/shm -v $KERNEL_REPO_HOME:/data/kernel -v $ANDROID_REPO_HOME:/data/aosp k-builder /data/aosp/kernel/tests/net/test/run_net_test.sh "$@"
